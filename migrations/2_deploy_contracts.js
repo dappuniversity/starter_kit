f2857be95a71f29b618deae2f6bff0ac6a5229fd
Lukasz <lukaszkkoziel@gmail.com>
2020-07-31 19:04:56 +0900

    eth contract created and tokens are ready

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require("Token");
+const EhtSwap = artifacts.require("EhtSwap");
+
+module.exports = async function(deployer) {
+  // DEPLOY
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+// DEPLOY
+  await deployer.deploy(EhtSwap);
+  const ehtSwap = await EhtSwap.deployed()
+
+// TRANSFER
+  await token.transfer(ehtSwap.address, '1000000000000000000000000')
+};
---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+/**
+ * The EhtSwap contract does this and that...
+ */
+contract EhtSwap {
+   string public name = "EhtSwap Instant Exchange";
+}
