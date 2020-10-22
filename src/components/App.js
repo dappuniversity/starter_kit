edbffe23770c55d3a2e025f4fdf997a8863c4cba
Syed Asim Raza Naqvi <asimraza243@gmail.com>
2020-10-23 02:41:54 +0500

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+// import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
@@ -20,25 +20,12 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
+               
                 <h1>Dapp University Starter Kit</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+               
               </div>
             </main>
           </div>
