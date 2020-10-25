1be2aaa9e9709aa10e4592ad8385c7e02a4d97d1
Syed Asim Raza Naqvi <asimraza243@gmail.com>
2020-10-25 21:29:28 +0500

    Update App.js and Web3

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,12 @@
-import React, { Component } from 'react';
-// import logo from '../logo.png';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-               
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-               
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App(){
+
+  return(
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
 }
 
 export default App;
