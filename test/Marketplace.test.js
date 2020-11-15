052b45ca256949da24ab955de7931e019a30af46
istott <isaac.cloyd@gmail.com>
2020-11-14 20:37:40 -0700

    feat: create two tests. One to test deployment and its address. another to verify the name is correct

---
+++ test/Marketplace.test.js
@@ -0,0 +1,25 @@
+const Marketplace = artifacts.require('./Marketplace.sol')
+
+contract('Marketplace', (accounts) => {
+    let marketplace 
+
+    before(async () => {
+        marketplace = await Marketplace.deployed()
+
+    })
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = await marketplace.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('has a name', async () => {
+            const name = await marketplace.name()
+            assert.equal(name, 'Dapp University Marketplace')
+        })
+    })
+})
