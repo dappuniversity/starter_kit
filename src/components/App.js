676ab70fb6467e9c7de0f6d6e132c78900987efb
istott <isaac.cloyd@gmail.com>
2020-11-17 20:54:05 -0700

    Feat: connect Ganache blockchain to MeteMask wallet.
    render blockchain account address on client side.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import Marketplace from '../abis/Marketplace.json';
+import NavBar from './NavBar';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+    // console.log(window.web3)
+
+  }
+
+  async loadWeb3() {
+
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // console.log(accounts)
+    const networkId = await web3.eth.net.getId()
+    const networkData = Marketplace.networks[networkId]
+    if(networkData) {
+      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
+      console.log(marketplace)
+    } else {
+      window.alert('Marketplace contract not deployed to detected network.')
+    }
+
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  
+    render() {
+      return (
+        <div>
+          <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a
+              className="navbar-brand col-sm-3 col-md-2 mr-0"
+              href="http://www.dappuniversity.com/bootcamp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dope love me long time
+            </a>
+          <ul className='navbar-nav px-3'>
+            <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+              <small className='text-white'><span id='account'>{this.props.account}</span></small>
+            </li>
+          </ul>
+          </nav>
+        </div>
+      );
+    }
+  }
+  
+  export default NavBar;
