915accda7e3889fa3e4c9fffbafb758908ebafbb
AsimRaza <57153665+SyedAsimRazaNaqvi@users.noreply.github.com>
2020-12-16 19:44:11 +0500

    edits

--- src/components/PropertyList.js
+++ src/components/PropertyList.js
@@ -7,7 +7,6 @@ import { property_detail } from "../store/actions";
 
 export const PropertyList = () => {
     const [events, setEvents] = useState([{}]);
-    // const [data, setData] = useState([{}])
     const [{ contract, accounts }, dispatch] = useStore();
 
 
@@ -15,17 +14,17 @@ export const PropertyList = () => {
         async function getData() {
             const response = await property_Detail(contract)
             setEvents(response)
-            // setData(returnValues)
+
         }
         getData();
     }, [])
-    let returnValues = [] //or it may be object
+    let returnValues = []
     const alldata = () => {
         events.map((item, index) => {
             return returnValues[index] = item.returnValues
 
         })
-     
+
         return returnValues
     }
     returnValues = alldata()
@@ -33,45 +32,26 @@ export const PropertyList = () => {
     return (
 
         <div>
-            {/* {console.log(returnValues)} */}
 
-            {/* {data()} */}
 
             {returnValues.map(item => {
-                for (var a in item){
-                  return  <div keys={item[0]}> 
-                  <h3>{item[0]}</h3>
-                  <h3>{item[1]}</h3>
-                  <h3>{item[2]}</h3>
-                  <h3>{item[3]}</h3>
-                  <h3>{item[4]}</h3>
-                  <h3>{item[5]}</h3>
-                  <h3>{item[6]}</h3>
-                  <h3>{item[7]}</h3>
-                  <h3>{item[8]}</h3>
-                  <br/>
-                  </div>
-                } })
+                for (var a in item) {
+                    return <div keys={item[0]}>
+                        <h3>{item[0]}</h3>
+                        <h3>{item[1]}</h3>
+                        <h3>{item[2]}</h3>
+                        <h3>{item[3]}</h3>
+                        <h3>{item[4]}</h3>
+                        <h3>{item[5]}</h3>
+                        <h3>{item[6]}</h3>
+                        <h3>{item[7]}</h3>
+                        <h3>{item[8]}</h3>
+                        <br />
+                    </div>
+                }
+            })
             }
-            
-                {/* // console.log(item[0].useraddress)
-                // return Object.keys(item).map((val, index) => {
-                //     console.log(index)
-                    // return Object.keys(returnValues[val]).map( data=>{
 
-                    //     return data.area
-                    // })
-
-                // })
-         //   })
-           // } */}
-
-            {/* {returnValues.map((index) => {
-                return Object.values(index).map((item, i) => {
-                    console.log(index["2"])
-                    return <li key={i}> {item}</li>
-                })
-            })} */}
             {alldata}
         </div>
 
