7bdaf5b3a1c49203a8dc7f35b1ed84daef3c042a
Amish Menon <amishmenon@gmail.com>
2022-05-31 17:13:30 -0500

    added contracts and migration scripts

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,23 +1,24 @@
-pragma solidity >=0.4.21 <0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
 
 contract Migrations {
-  address public owner;
+  address public owner = msg.sender;
   uint public last_completed_migration;
 
-  constructor() public {
-    owner = msg.sender;
-  }
-
   modifier restricted() {
-    if (msg.sender == owner) _;
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
   }
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
     upgraded.setCompleted(last_completed_migration);
   }
 }
