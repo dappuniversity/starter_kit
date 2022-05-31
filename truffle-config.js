310a87e65fd87d22cd90222f833924d23be73788
Amish Menon <amishmenon@gmail.com>
2022-05-31 17:23:41 -0500

    updated configs and removed image

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,22 @@
-require('babel-register');
-require('babel-polyfill');
+require("babel-register");
+require("babel-polyfill");
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
     },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+        runs: 200,
+      },
+    },
+  },
+};
