2801663d7a5a467ee609b1301a135c5764045132
Ceren Yıldırım <cerenyildirim@Ceren-MacBook-Air.local>
2022-06-09 20:22:10 +0300

    Voting page integrated with blockchain.

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,68 +1,97 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-
+import Signatures from '../abis/Signatures.json'
+import  { ethers } from "ethers"
 import "./Vote.css";
 
 export default class Vote extends React.Component  {
-    toggle() {
-        var x = document.getElementById("myDIV");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
+
+
+  constructor() {
+    super();
+    this.state = {
+      contract: "",
+      signatures: [],
+    };
+
+    this.loadBlockchainData = this.loadBlockchainData.bind(this);
+    this.getSignatures = this.getSignatures.bind(this);
+  
+}
+
+async componentWillMount() {
+  await this.loadBlockchainData();
+  await this.getSignatures();
+}
+
+loadBlockchainData = async () => {
+   
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const networkData = Signatures.networks[5777];
+    const signatureContract = new ethers.Contract(networkData.address, Signatures.abi, signer);
+    await this.setState({contract: signatureContract});
+
+}
+}
+
+getSignatures = async () => {
+  const files = await this.state.contract.getFiles();
+  let recArr = []
+  files.forEach(file => {
+    recArr.push({
+      id: file.id.toNumber(),
+      owner: file.owner,
+      attackVote: file.attackVote.toNumber(),
+      normalVote: file.normalVote.toNumber(),
+      ipfsHash: file.ipfsHash,
+    });
+  })
+  this.setState({signatures: recArr});
+  console.log(this.state.signatures)
+}
+
+
+
 
 
   render () {
-    let layout = (
-    <div className="main" id="content">
-        <div className="Page">
-        <h2>Vote Signatures</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Signature</th>
-              <th scope="col">Votes</th>
-              <th scope="col">Owner</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody id="productList">
-            <tr>
-              <th scope="row">1</th>
-              <td>Signature #1</td>
-              <td>5 safe 1 malicious</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="downButton">Download data</button></td>
-              <td><button className="voteButton">Vote</button></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Signature #2</td>
-              <td>3 safe 2 malicious</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="downButton">Download data</button></td>
-              <td><button className="voteButton">Vote</button></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Signature #3</td>
-              <td>0 safe 2 malicious</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="downButton">Download data</button></td>
-              <td><button className="voteButton">Vote</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      </div>
-  );
+
     return (
-        <div>
-            {layout}
-        </div>
+      <div className="main" id="content">
+      <div className="Page">
+      <h2>Vote Signatures</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Signature</th>
+            <th scope="col">Votes</th>
+            <th scope="col">Owner</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+        { this.state.signatures.map((file, key) => {
+              return(
+                <tr key={key}>
+                  <th scope="row">{file.id}</th>
+              <td>Signature #{file.id}</td>
+              <td>{file.normalVote} normal, {file.attackVote} malicious</td>
+              <td>{file.owner}</td>
+              <td><button className="downButton"><p><a href={"https://ipfs.io/ipfs/" + file.ipfsHash + "/"}>Download data</a></p></button></td>
+              <td><button className="voteButton">Vote</button></td>
+              </tr>
+              )
+            })}
+        </tbody>
+      </table>
+    </div>
+    </div>
       );
     }
 };
+
