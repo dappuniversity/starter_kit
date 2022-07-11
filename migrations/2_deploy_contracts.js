0b21b0c048dc52498ae40801d45100b3124a159f
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-11 14:30:14 +0530

    deploying token to blockchain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+
+    await deployer.deploy(Token);
+    const token=await Token.deployed();
+
+    await deployer.deploy(EthSwap);
+    const ethSwap=await Token.deployed();
+
+    await token.transfer(ethSwap.address,'1000000000000000000000000')
 };
