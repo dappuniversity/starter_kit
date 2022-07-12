81fdd2a74f9c3d71f5c23fad84b51b0b2bc2cfd8
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-12 23:59:43 +0530

    done

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = async function(deployer) {
     await deployer.deploy(Token);
     const token=await Token.deployed();
 
-    await deployer.deploy(EthSwap);
+    await deployer.deploy(EthSwap,token.address);
     const ethSwap=await Token.deployed();
 
     await token.transfer(ethSwap.address,'1000000000000000000000000')
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,19 +1,24 @@
 const { assert } = require('chai');
 
 
+
 const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 
 require('chai')
     .use(require('chai-as-promised'))
-    .should()
+    .should();
 
-contract('EthSwap',(accounts)=>{
+    function tokens(n) {
+        return web3.utils.toWei(n, 'ether');
+    }
+
+contract('EthSwap',([deployer,investor])=>{
     let token,ethSwap
     before(async ()=>{
         token = await Token.new()
-        ethSwap = await EthSwap.new()
-        await token.transfer(ethSwap.address,'1000000000000000000000000')
+        ethSwap = await EthSwap.new(token.address)
+        await token.transfer(ethSwap.address,tokens('1000000'))
     })
 
     describe('Token deployment',async()=>{
@@ -30,7 +35,32 @@ contract('EthSwap',(accounts)=>{
 
         it('contract has tokens',async()=>{
             let balance=await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(),'1000000000000000000000000')
+            assert.equal(balance.toString(),tokens('1000000'))
+        })
+    })
+
+    describe('buy tokens',async()=>{
+        let result
+
+        before(async () => {
+          // Purchase tokens before each example
+          result = await ethSwap.buyTokens({ from: investor, value: web3.utils.toWei('1', 'ether')})
+        })
+        it('Allows user to buy tokens for a fixed eth price',async()=>{
+            let investorBalance=await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(),tokens('100'))
+
+            let ethSwapBalance=await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(),tokens('999900'))
+            ethSwapBalance=await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(),web3.utils.toWei('1','Ether'))
+
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
         })
     })
 })
