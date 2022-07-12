c8e0cb43fa7f329ba7d9f6a531b9addc00752943
Sahitya77 <sahityanijhawan@gmail.com>
2022-07-12 23:59:35 +0530

    buy tokens complete

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,6 +1,33 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap{
     string public name="EthSwap Instant Exchange";
-    
+    Token public token;
+    uint public rate=100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor (Token _token) public {
+        token = _token;
+
+    }
+
+    function buyTokens() public payable{
+        uint tokenAmount = msg.value * rate;
+
+        require(token.balanceOf(address(this)) >= tokenAmount);
+        token.transfer(msg.sender,tokenAmount);
+
+        emit TokenPurchased(msg.sender,address(token), tokenAmount, rate);
+    }
+    function sellTokens(){
+        
+    }
 }
